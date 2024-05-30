9dffd600bbd38cc7b0935eae5a694b4225d76fb1
최민혁 <mhchoi1997@inswave.com>
2024-05-30 09:52:46 +0900

    샘플 변경

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ var proxy = require("./server.js");
 var express = require('express');
 
 var app = express();
+// 포트 지정
 var port = (process.env.PORT || 3000);
 
+// 미들 웨어
 app.use("/", proxy());
 
-console.log("Server running on port", port);
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server runing on port --- ${port}`)
+})
